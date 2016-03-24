6dc94c6525d0a6d77a1c3d5a9fa2f27c42e7828d
Erin Kim <kim.erin.kim@gmail.com>
2016-03-24 15:06:24 -0400

    enabled student and instructor to greet

--- people.rb
+++ people.rb
@@ -2,6 +2,10 @@ class Person
   def initialize(name)
     @name = name
   end
+
+  def greeting
+    puts "Hi, my name is #{@name}."
+  end
 end
 
 class Student < Person
@@ -15,3 +19,15 @@ class Instructor < Person
     puts "Everything in Ruby is an Object."
   end
 end
+
+
+instructor = Instructor.new("Chris")
+instructor.greeting
+
+student = Student.new("Cristina")
+student.greeting
+
+instructor.teach
+student.teach
+# you can't call teach method on student
+# because the teach method is instructor's behaviour not the student
