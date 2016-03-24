7ec3e265194612bd12b217493218e02b35773b46
Erin Kim <kim.erin.kim@gmail.com>
2016-03-24 14:32:38 -0400

    Added Parent Person class

--- people.rb
+++ people.rb
@@ -1,10 +1,16 @@
-class Student
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Student < Person
  def learn
    puts "I get it!"
  end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object."
   end
